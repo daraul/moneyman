5a2a0d3210197ec15cd9682f3435566e92b8918d
daraul <daraul@intellectstorm.com>
2017-05-07 16:16:04 -0400

    Add material2 and some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MdSidenavModule, MdButtonModule, MdIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdSidenavModule, MdButtonModule, MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
